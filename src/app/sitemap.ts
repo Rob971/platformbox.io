9e18fd7693527c28ce46aa3673f8d79290e7e582
Roberto CORNANO <roberto.cornano@mymultiverse.app>
2026-09-08 12:42:42 +0200

    feat: add assessment route to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,6 +9,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
+      url: "https://www.platformbox.io/assessment",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
       url: "https://www.platformbox.io/showcase",
       lastModified: new Date(),
       changeFrequency: "monthly",
